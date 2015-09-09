c249ecd63396013bda73da6f982c4cb6cf879fd6
Steven Myint <git@stevenmyint.com>
2015-09-08 18:34:31 -0700

    Use argparse

--- scspell.py
+++ scspell.py
@@ -17,12 +17,12 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-"""scspell -- an interactive, conservative spell-checker for source code."""
+"""An interactive, conservative spell-checker for source code."""
 
 from __future__ import absolute_import
 from __future__ import print_function
 
-import optparse
+import argparse
 import sys
 import uuid
 
@@ -30,60 +30,49 @@ import scspell
 
 
 def main():
-    parser = optparse.OptionParser(usage="""\
-    %prog [options] [source files]
+    parser = argparse.ArgumentParser(description=__doc__, prog='scspell')
 
-    Performs spell checking on all of the [source files].""",
-                                   version="""\
-    %%prog v%s
-    Copyright (C) 2009 Paul Pelzl
-
-    %%prog comes with ABSOLUTELY NO WARRANTY.  This is free software, and
-    you are welcome to redistribute it under certain conditions; for details,
-    see COPYING.txt as distributed with the program.
-    """ % scspell.__version__)
-
-    spell_group = optparse.OptionGroup(parser, 'Spell Checking')
-    spell_group.add_option(
+    parser.add_argument(
         '--override-dictionary', dest='override_filename',
         help='set location of dictionary to FILE, for current session only',
         metavar='FILE', action='store')
-    spell_group.add_option('--report-only', dest='report', action='store_true',
-                           help='Non-interactive report of spelling errors')
-    parser.add_option_group(spell_group)
-
-    config_group = optparse.OptionGroup(parser, 'Configuration')
-    config_group.add_option(
+    parser.add_argument('--report-only', dest='report', action='store_true',
+                           help='non-interactive report of spelling errors')
+    parser.add_argument(
         '--set-dictionary', dest='dictionary',
         help='permanently set location of dictionary to FILE', metavar='FILE',
         action='store')
-    config_group.add_option(
+    parser.add_argument(
         '--export-dictionary', dest='export_filename',
         help='export current dictionary to FILE', metavar='FILE',
         action='store')
-    parser.add_option_group(config_group)
-
-    parser.add_option('-i', '--gen-id', dest='gen_id', action='store_true',
+    parser.add_argument('-i', '--gen-id', dest='gen_id', action='store_true',
                       help='generate a unique file-id string')
-    parser.add_option('-D', '--debug', dest='debug', action='store_true',
+    parser.add_argument('-D', '--debug', dest='debug', action='store_true',
                       help='print extra debugging information')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s ' + scspell.__version__)
+    parser.add_argument('files', nargs='+', help='files to check')
 
-    (opts, files) = parser.parse_args()
-    if opts.debug:
+    args = parser.parse_args()
+
+    if args.debug:
         scspell.set_verbosity(scspell.VERBOSITY_MAX)
 
-    if opts.gen_id:
+    if args.gen_id:
         print('scspell-id: %s' % str(uuid.uuid1()))
-    elif opts.dictionary is not None:
-        scspell.set_dictionary(opts.dictionary)
-    elif opts.export_filename is not None:
-        scspell.export_dictionary(opts.export_filename)
-        print("Exported dictionary to '{}'".format(opts.export_filename),
+    elif args.dictionary is not None:
+        scspell.set_dictionary(args.dictionary)
+    elif args.export_filename is not None:
+        scspell.export_dictionary(args.export_filename)
+        print("Exported dictionary to '{}'".format(args.export_filename),
               file=sys.stderr)
-    elif len(files) < 1:
+    elif len(args.files) < 1:
         parser.error('No files specified')
     else:
-        okay = scspell.spell_check(files, opts.override_filename, opts.report)
+        okay = scspell.spell_check(args.files,
+                                   args.override_filename,
+                                   args.report)
         return 0 if okay else 1
 
 
